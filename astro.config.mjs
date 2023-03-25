fe62433cd382257cb8a428c90b8fb1c6534a0690
nienow <system@randombits.dev>
2023-03-25 18:32:23 -0500

    Added code titles remark plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import solid from '@astrojs/solid-js';
+import remarkCodeTitles from 'remark-code-titles';
+
 
 export default defineConfig({
     base: '/',
     site: 'https://randombits.dev/',
     markdown: {
-        syntaxHighlight: 'prism'
+        syntaxHighlight: 'prism',
+        remarkPlugins: [remarkCodeTitles]
     },
     integrations: [mdx(), solid()],
     build: {
