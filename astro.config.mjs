1d1d29bce6371eea600126ccd38f0b9df1f771b3
Matthew Nienow <mnienow@pm.me>
2023-06-12 16:57:09 -0500

    Added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import solid from '@astrojs/solid-js';
 import remarkCodeTitles from 'remark-code-titles';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-    base: '/',
-    site: 'https://randombits.dev/',
-    markdown: {
-        syntaxHighlight: 'prism',
-        remarkPlugins: [remarkCodeTitles]
-    },
-    integrations: [mdx(), solid()],
-    build: {
-        assets: '_astro'
-    },
-    experimental: {
-        assets: true
-    }
+  base: '/',
+  site: 'https://randombits.dev/',
+  markdown: {
+    syntaxHighlight: 'prism',
+    remarkPlugins: [remarkCodeTitles]
+  },
+  integrations: [mdx(), solid(), tailwind()],
+  build: {
+    assets: '_astro'
+  },
+  experimental: {
+    assets: true
+  }
 });
