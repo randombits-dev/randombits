8a3373d9e22d96e6c116030c6c03ef1c56821b79
Matthew Nienow <mnienow@pm.me>
2023-11-05 07:36:48 -0600

    Fixed width on mobile

--- src/components/layout/ThemeChanger.tsx
+++ src/components/layout/ThemeChanger.tsx
@@ -2,56 +2,56 @@ import {createSignal, onMount} from "solid-js";
 import '@/styles/theme-changer.scss';
 
 const THEME_LIST = [
-    {id: "base", name: "Bumble"},
-    {id: "daring", name: "Bold"},
-    {id: "gray", name: "Overcast"},
-    {id: "dark-fear", name: "Midnight"},
+  {id: "base", name: "Bumble"},
+  {id: "daring", name: "Bold"},
+  {id: "gray", name: "Overcast"},
+  {id: "dark-fear", name: "Midnight"},
 ];
 
 const ThemeChanger = () => {
-    const [currentTheme, setCurrentTheme] = createSignal('');
+  const [currentTheme, setCurrentTheme] = createSignal('');
 
-    const changeTheme = (newTheme: any) => {
-        const htmlClass = document.documentElement.classList;
-        htmlClass.remove(`theme-${currentTheme()}`, 'theme-dark');
+  const changeTheme = (newTheme: any) => {
+    const htmlClass = document.documentElement.classList;
+    htmlClass.remove(`theme-${currentTheme()}`, 'theme-dark');
 
-        setCurrentTheme(newTheme.id);
-        if (newTheme.id.startsWith('dark-')) {
-            document.documentElement.classList.add(`theme-${newTheme.id}`, `theme-dark`);
-        } else {
-            document.documentElement.classList.add(`theme-${newTheme.id}`);
-        }
-        localStorage.setItem("theme", newTheme.id);
-    };
+    setCurrentTheme(newTheme.id);
+    if (newTheme.id.startsWith('dark-')) {
+      document.documentElement.classList.add(`theme-${newTheme.id}`, `theme-dark`);
+    } else {
+      document.documentElement.classList.add(`theme-${newTheme.id}`);
+    }
+    localStorage.setItem("theme", newTheme.id);
+  };
 
-    onMount(() => {
-      const initialTheme = localStorage.getItem("theme") || 'base';
-      setCurrentTheme(initialTheme);
-    });
+  onMount(() => {
+    const initialTheme = localStorage.getItem("theme") || 'base';
+    setCurrentTheme(initialTheme);
+  });
 
-    return (
-        <div class="static md:fixed w-full bottom-[-21px] text-center z-20">
-        <div class="rb-theme-changer inline-block md:rounded-t-xl overflow-hidden bg-header text-header-text">
-            {THEME_LIST.map((theme) => {
-                const btnClasses = "rounded-none inline-block px-5 py-3 mb-4 justify-center font-bold";
-                if (currentTheme() === theme.id) {
-                    return (
-                        <button class={btnClasses + ' rb-theme-active'}>
-                            {theme.name}
-                        </button>
-                    );
-                } else {
-                    return (
-                        <button class={btnClasses} onClick={() => changeTheme(theme)}>
-                            {theme.name}
-                        </button>
-                    );
-                }
+  return (
+    <div class="fixed md:fixed w-full bottom-[-21px] text-center z-20">
+      <div class="rb-theme-changer inline-block md:rounded-t-xl overflow-hidden bg-header text-header-text">
+        {THEME_LIST.map((theme) => {
+          const btnClasses = "rounded-none inline-block px-5 py-3 mb-4 justify-center font-bold";
+          if (currentTheme() === theme.id) {
+            return (
+              <button class={btnClasses + ' rb-theme-active'}>
+                {theme.name}
+              </button>
+            );
+          } else {
+            return (
+              <button class={btnClasses} onClick={() => changeTheme(theme)}>
+                {theme.name}
+              </button>
+            );
+          }
 
-            })}
-        </div>
-        </div>
-    );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ThemeChanger;
