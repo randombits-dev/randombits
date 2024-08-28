cd81e1fe5ef73c896fbd2e331a69319b9c01fee2
nienow <mnienow@pm.me>
2024-08-27 20:32:52 -0500

    Update dependencies

---
+++ src/data/news.ts
@@ -0,0 +1,14 @@
+export const NEWS: { date: string, title: string, desc: string, link: string }[] = [
+  {
+    date: '2024-06-05',
+    title: 'Excalidraw Plugin (Standard Notes)',
+    desc: 'Release 1.1.0 - Added light/dark mode toggle on main page, and it now saves your setting',
+    link: 'https://github.com/nienow/sn-excalidraw'
+  },
+  {
+    date: '2024-06-05',
+    title: 'Excalidraw Plugin (Standard Notes)',
+    desc: 'Release 1.1.0 - Added light/dark mode toggle on main page, and it now saves your setting',
+    link: 'https://github.com/nienow/sn-excalidraw'
+  }
+];
